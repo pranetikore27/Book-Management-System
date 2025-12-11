2db117dffc992ba3ab2e3037e3bfde80e70ac437
Praneti <pranetipkore@gmail.com>
2025-12-11 22:28:51 +0530

    Folder structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/praneti/Book-Management-System
+
+go 1.24.6
+
+require github.com/gorilla/mux v1.8.1
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/praneti/book-management-system/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookByID).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
