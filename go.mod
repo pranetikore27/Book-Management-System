39f6f7aa0c28fe0c0b6363eb6e20ad6210143068
Praneti <pranetipkore@gmail.com>
2025-12-12 06:42:24 +0530

    Functions created to  Create, Update, Fetch

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/pranetikore27/Book-Management-System
+module github.com/pranetikore27/book-management-system
 
-go 1.24.6
+go 1.24
 
 require github.com/gorilla/mux v1.8.1
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/pranetikore27/Book-Management-System/pkg/controllers"
+	"github.com/pranetikore27/book-management-system/pkg/controllers"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookByID).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
